a940ba72a38820baf950d48f443c7292f283b0dd
Saurabh Saneja <saurabh250183@gmail.com>
2023-10-10 16:37:56 +0530

    minor

--- src/components/AccordionItem/AccordionItemStyle.js
+++ src/components/AccordionItem/AccordionItemStyle.js
@@ -56,7 +56,7 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    width: '80%',
+    width: '90%',
   },
   leftSubRow: {
     flexDirection: 'row',
